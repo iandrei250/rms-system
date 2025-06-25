558181e158429675cbc8c53d1659219db8502ba7
iandrei250 <aiordache020@gmail.com>
2025-06-25 12:52:34 +0200

    update: display all devices

---
+++ rms-system/src/components/Home/DeviceCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="rounded overflow-hidden shadow-lg hover:bg-gray-200 hover:cursor-pointer"
+  >
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ device.name }}</div>
+      <p class="text-gray-700 text-base">{{ device.serialNumber }}</p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >{{ device.connectionStatus }}</span
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Device } from "../../helpers/types/types";
+
+defineProps<{
+  device: Device;
+}>();
+</script>
--- rms-system/src/pages/HomePage.vue
+++ rms-system/src/pages/HomePage.vue
@@ -1,12 +1,20 @@
-<template></template>
+<template>
+  <div class="grid grid-cols-3">
+    <div class="p-4" v-for="device in devices">
+      <DeviceCard :key="device.id" :device="device" />
+    </div>
+  </div>
+</template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { getDevices } from "../helpers/api/apiRequests";
+import type { Device } from "../helpers/types/types";
+import DeviceCard from "../components/Home/DeviceCard.vue";
+
+const devices = ref<Device[]>([]);
 
 onMounted(async () => {
-  const devices = await getDevices();
-
-  console.log("Devices:", devices);
+  devices.value = await getDevices();
 });
 </script>
